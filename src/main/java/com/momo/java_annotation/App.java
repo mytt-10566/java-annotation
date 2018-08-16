0e7872371c6e1438be0ed7060dcc59033247aa38
mytt-10566 <mo1056610188@163.com>
2018-08-16 18:06:21 +0800

    首次提交

---
+++ src/main/java/com/momo/java_annotation/App.java
@@ -0,0 +1,13 @@
+package com.momo.java_annotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
